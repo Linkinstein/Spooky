d4fbd89505129ae0068ddc1f5e3e0956894a5fb3
Linkinstein <78932362+Linkinstein@users.noreply.github.com>
2024-05-04 16:14:54 +1000

    Objective Progression Revamp

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
     [SerializeField] public ItemData item;
-
+    [Space]
     [SerializeField] private bool willProgress;
+    [SerializeField] private int progressIndex = 0;
     [Space]
     [SerializeField] private bool keyItem;
     [SerializeField] private bool door;
     [SerializeField] private bool consumable;
     [SerializeField] private bool note;
+
+    private GameManager gm
+    { get { return GameManager.Instance; } }
 
     public string iName
     { get { return item.itemName; } }
@@ -31,7 +35,7 @@ public class Interactable : MonoBehaviour
 
         if (door) 
         {
-            foreach (ItemData id in GameManager.Instance.keyItems)
+            foreach (ItemData id in gm.keyItems)
             {
                 if (id.itemName.Equals(item.itemName))
                 {
@@ -43,9 +47,9 @@ public class Interactable : MonoBehaviour
 
         if (keyItem || consumable || note)
         {
-            if (keyItem) GameManager.Instance.keyItems.Add(item);
-            if (consumable) GameManager.Instance.consumables.Add(item);
-            if (note) GameManager.Instance.notes.Add(item);
+            if (keyItem) gm.keyItems.Add(item);
+            if (consumable) gm.consumables.Add(item);
+            if (note) gm.notes.Add(item);
 
             if (willProgress) Progress();
             Destroy(gameObject);
@@ -55,7 +59,10 @@ public class Interactable : MonoBehaviour
 
     private void Progress()
     {
-        if (GameManager.Instance != null) GameManager.Instance.objectivesIndex++;
+        if (gm != null)
+        {
+            if (gm.objectivesIndex<progressIndex) gm.objectivesIndex = progressIndex;
+        } 
         
     }
 }
